668a86e052a6878590ccc852ed2291371d08c6a5
Yoga Wahyudi <yogawahyudi7@gmail.com>
2022-07-09 17:30:24 +0700

    learnJava

---
+++ src/com/helloWorld/Main.java
@@ -0,0 +1,20 @@
+package com.helloWorld;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("THAX BOY");
+
+        float i = 10;
+        float j = 4;
+        float result = i % j; 
+        System.out.println(result);
+
+
+        int a = 16;
+        int b = 3;
+        float c = (float)a / b; // result = 5.3333
+        System.out.println(c);
+    }
+
+}
